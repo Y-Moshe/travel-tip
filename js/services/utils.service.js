1df484849908d36e760a744716c48d8923fba94d
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-02 15:41:13 +0200

    utils service

---
+++ js/services/utils.service.js
@@ -0,0 +1,30 @@
+function makeId(length = 6) {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  var txt = ''
+  for (var i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return txt
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
+
+function updateQueryParam(key, value) {
+  const queryParams = new URLSearchParams(window.location.search);
+  queryParams.set(key, value);
+
+  history.pushState(null, null, '?' + encodeURI(queryParams.toString()));
+}
