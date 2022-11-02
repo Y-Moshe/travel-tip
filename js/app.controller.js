b53f27a6abce2fdd267a30c1c3e6f374bd267da2
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-02 19:11:58 +0200

    onSearch, renderLocations

--- js/app.controller.js
+++ js/app.controller.js
@@ -3,16 +3,33 @@ import { mapService } from './services/map.service.js'
 
 window.onload = onInit
 window.onAddMarker = onAddMarker
-window.onPanTo = onPanTo
-window.onGetLocs = onGetLocs
 window.onGetUserPos = onGetUserPos
+window.onSearch = onSearch
 
 function onInit() {
     mapService.initMap()
         .then(() => {
             console.log('Map is ready')
+            renderLocations()
         })
         .catch(() => console.log('Error: cannot init map'))
+}
+
+function onSearch(ev) {
+    ev.preventDefault()
+
+    const searchInput = document.querySelector('.location-search input[type="search"]').value
+    mapService.getLocByKeyword(searchInput)
+        .then(({ location, formattedAddress }) => {
+            mapService.panTo(location.lat, location.lng, 10)
+            // TODO open InfoWindow
+            updateTitle(formattedAddress)
+        })
+
+}
+
+function updateTitle(txt) {
+    document.querySelector('.current-location span').innerText = txt
 }
 
 function onAddMarker() {
@@ -20,16 +37,44 @@ function onAddMarker() {
     mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 })
 }
 
-function onGetLocs() {
+function renderLocations() {
     locService.getLocs()
         .then(locs => {
+            const strLocs = locs.map(renderLocation).join('')
             console.log('Locations:', locs)
-            document.querySelector('.locs').innerText = JSON.stringify(locs, null, 2)
+
+            document.querySelector('.saved-locations')
+                .innerHTML = strLocs
         })
 }
 
+function renderLocation({ id, name, formattedAddress }) {
+    return `
+        <article class="location-card">
+            <h3>${name}</h3>
+            <h5>${formattedAddress}</h5>
+            <button class="btn-go" onclick="onGoTo('${id}')">GO</button>
+            <button class="btn-share" onclick="onShare('${id}')">Share</button>
+            <button class="btn-delete" onclick="onDelete('${id}')">Delete</button>
+        </article>
+    `
+}
+
+function onGoTo(locationId) {
+    console.log('Panning the Map')
+    mapService.panTo(lat, lng)
+}
+
+function onShare() {
+    
+}
+
+function onDelete() {
+
+}
+
 function onGetUserPos() {
-    locService.getCurrPosition()
+    getCurrPosition()
         .then(({ coords }) => {
             console.log('User position is:', coords)
             mapService.panTo(coords.latitude, coords.longitude)
@@ -42,7 +87,10 @@ function onGetUserPos() {
         })
 }
 
-function onPanTo() {
-    console.log('Panning the Map')
-    mapService.panTo(35.6895, 139.6917)
+// This function provides a Promise API to the callback-based-api of getCurrentPosition
+function getCurrPosition() {
+    console.log('Getting Pos')
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
 }
