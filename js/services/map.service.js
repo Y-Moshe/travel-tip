9275dc1401e5dd97026672650a6ef7d5647545ec
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-02 16:27:14 +0200

    removeMarker, getLocByKeyword methods, loadMarkers

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -1,12 +1,20 @@
+import { locService } from './loc.service.js'
 export const mapService = {
     initMap,
+    panTo,
     addMarker,
-    panTo
+    removeMarker,
+    getLocByKeyword
 }
 
+const API_KEY = 'AIzaSyBL3ZxetNDgUPAs_yW57YL8pXknqKOfo7k'
 
 // Var that is used throughout this Module (not global)
 var gMap
+var gMarkers = []
+// [
+//     { id, marker }
+// ]
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
     console.log('InitMap')
@@ -18,17 +26,35 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
                 center: { lat, lng },
                 zoom: 15
             })
+            loadMarkers()
             console.log('Map!', gMap)
         })
 }
 
-function addMarker(loc) {
+function loadMarkers() {
+    locService.getLocs()
+        .then(locs => {
+        locs.forEach(({ id, name, lat, lng }) => {
+            const pos = { lat, lng }
+            gMarkers.push({ id, marker: addMarker(pos, name) })
+        })
+    })
+}
+
+function addMarker(pos, name) {
     var marker = new google.maps.Marker({
-        position: loc,
+        position: pos,
         map: gMap,
-        title: 'Hello World!'
+        title: name
     })
     return marker
+}
+
+function removeMarker(id) {
+    const idx = gMarkers.findIndex(marker => marker.id === id)
+    const mark = gMarkers[idx].marker
+    mark.setVisible(false)
+    gMarkers.splice(idx, 1)[0]
 }
 
 function panTo(lat, lng, zoom = 8) {
@@ -37,9 +63,21 @@ function panTo(lat, lng, zoom = 8) {
     gMap.setZoom(zoom)
 }
 
+function getLocByKeyword(keyword) {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${keyword}&key=${API_KEY}`
+    fetch(url)
+        .then(res => res.json())
+        .then(({ results }) => {
+            const { geometry, formatted_address } = results
+            return {
+                formattedAddress: formatted_address,
+                location: geometry.location
+            }
+        })
+}
+
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = 'AIzaSyBL3ZxetNDgUPAs_yW57YL8pXknqKOfo7k'
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
